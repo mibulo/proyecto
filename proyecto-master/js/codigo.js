43eb9d931cb36ea6599d1a0bf75ffb85d8c49c09
rmoresco <73404944+rmoresco@users.noreply.github.com>
2022-10-24 07:22:00 -0300

    Add files via upload

---
+++ proyecto-master/js/codigo.js
@@ -0,0 +1,48 @@
+document.getElementById("idheader").innerHTML = `
+<div class="header-logo">
+                    <img src="img/logo3.jpg" width="200px" height="200px" alt="">
+
+ </div>
+                <div class="header-nav">
+                    <input type="checkbox" id="menu-check">
+                    <label id="menu" for="menu-check">
+                            <span id="menu-abrir">&#9776;</span>
+                            <span id="menu-cerrar" >X </span>
+                   </label>
+                    <nav>
+
+                        <ul>
+
+                            <div class="menu-nav">
+
+                                <div>
+                                    <li><a href="index.html">Home</a></li>
+                                </div>
+                                <div>
+                                    <li><a href="propiedades.html">Propiedades</a></li>
+                                </div>
+                                <div>
+                                    <li><a href="servicios.html">Servicios</a></li>
+                                </div>
+                                <div>
+                                    <li><a href="novedades.html">Novedades</a></li>
+                                </div>
+                                <div>
+                                    <li><a href="contacto.html">Contacto</a></li>
+                                </div>
+                            </div>
+                        </ul>
+                    </nav>
+                </div>
+`
+
+document.getElementById("idfooter").innerHTML = `
+<ul>
+<li><span>Todos los Derechos Reservados @2022 Bulo Real State</span></li>
+<li><a href="https://www.instagram.com" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
+<li>
+    <a href="https://www.facebook.com" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
+<li>
+    <a href="https://www.linkedin.com" target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
+</ul>
+`
